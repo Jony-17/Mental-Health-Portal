53f95f7ef225d630de6fd2723d0be5de578b04d6
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-03-09 01:28:09 +0000

    Add files via upload

--- areacliente/login/js/toggle-password.js
+++ areacliente/login/js/toggle-password.js
@@ -1,12 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const togglePassword = document.querySelector(".toggle-password");
-  const password = document.getElementById("password");
+document.addEventListener('DOMContentLoaded', function () {
+  const togglePassword = document.querySelector('.toggle-password');
+  const password = document.getElementById('password');
 
-  togglePassword.addEventListener("click", function () {
-    const type =
-      password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-    this.classList.toggle("fa-eye");
-    this.classList.toggle("fa-eye-slash");
+  togglePassword.addEventListener('click', function () {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
   });
 });
