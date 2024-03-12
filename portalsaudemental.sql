3f94b66b89f58b76223f6450e93fcfc9127e08a9
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-03-12 18:45:56 +0000

    Add files via upload

--- portalsaudemental.sql
+++ portalsaudemental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26-Fev-2024 às 18:36
+-- Tempo de geração: 12-Mar-2024 às 19:44
 -- Versão do servidor: 10.4.18-MariaDB
 -- versão do PHP: 8.0.3
 
@@ -24,11 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `mensagens`
+--
+
+CREATE TABLE `mensagens` (
+  `msg_id` int(11) NOT NULL,
+  `incoming_msg_id` int(255) NOT NULL,
+  `outgoing_msg_id` int(255) NOT NULL,
+  `msg` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `mensagens`
+--
+
+INSERT INTO `mensagens` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`) VALUES
+(1, 1703689423, 1703689628, 'ola'),
+(2, 1703689628, 1703689423, 'esta tudo bem nando?'),
+(3, 1703689423, 1703689628, 'sim e contigo?'),
+(4, 1703689628, 1703689423, 'também, obrigado'),
+(5, 1703689423, 1703689628, 'ok'),
+(6, 1703689628, 1035944128, 'Boa tarde Fernando!'),
+(7, 1035944128, 1703689628, 'Como estás?'),
+(8, 1703689628, 1035944128, 'a'),
+(9, 1703689628, 1035944128, 'a'),
+(10, 1703689628, 1035944128, 'a'),
+(11, 1703689628, 1035944128, 'a'),
+(12, 1703689628, 1035944128, 'a'),
+(13, 1703689628, 1035944128, 'a'),
+(14, 1703689628, 1035944128, 'a'),
+(15, 1703689628, 1035944128, 'a'),
+(16, 1703689628, 1035944128, 'a'),
+(17, 1703689628, 1035944128, 'a'),
+(18, 1703689628, 1035944128, 'a'),
+(19, 1703689628, 1035944128, 'a'),
+(20, 1703689628, 1035944128, 'a');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `utilizadores`
 --
 
 CREATE TABLE `utilizadores` (
   `utilizador_id` int(11) NOT NULL,
+  `unique_id` int(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -42,15 +82,22 @@ CREATE TABLE `utilizadores` (
 -- Extraindo dados da tabela `utilizadores`
 --
 
-INSERT INTO `utilizadores` (`utilizador_id`, `nome`, `email`, `password`, `genero`, `img_perfil`, `admin`, `data_criacao`) VALUES
-(10, 'Admin2', 'admin2@gmail.com', '$2y$10$/VvdG5/dxdJXeUa/ockQFeiuQtyPZPuIr0aZHd3u7QJVGa9Bg2ZKC', 2, 'user2.png', 1, '0000-00-00 00:00:00'),
-(12, 'Admin', 'admin@gmail.com', '$2y$10$qhuOmrEAp7dt2CkDG.RAiOqrT.Ief55PRldhZjDpzDuVejQv002k.', 1, 'user.png', 1, '2024-02-25 20:17:56'),
-(13, 'Antonio', 'antonio@gmail.com', '$2y$10$UBeLvYvpeSxePJjelsGKjOox.2RIbxVCAOEcTWmp8wB.EHucKmA6.', 1, '', 0, '2024-02-25 20:18:53'),
-(14, 'teste', 'teste@gmail.com', '$2y$10$sZvM0JFGYjzBnVk2ZFnEh.oK2.d2j8Yj3XumKEkqUiTEDgo2gw9yO', 1, '', 0, '2024-02-26 11:30:18');
+INSERT INTO `utilizadores` (`utilizador_id`, `unique_id`, `nome`, `email`, `password`, `genero`, `img_perfil`, `admin`, `data_criacao`) VALUES
+(10, 0, 'Admin2', 'admin2@gmail.com', '$2y$10$/VvdG5/dxdJXeUa/ockQFeiuQtyPZPuIr0aZHd3u7QJVGa9Bg2ZKC', 2, 'user2.png', 1, '0000-00-00 00:00:00'),
+(12, 0, 'Admin', 'admin@gmail.com', '$2y$10$qhuOmrEAp7dt2CkDG.RAiOqrT.Ief55PRldhZjDpzDuVejQv002k.', 1, 'user.png', 1, '2024-02-25 20:17:56'),
+(18, 1703689423, 'João', 'teste@gmail.com', '$2y$10$7Hf76z0PQaMFE.hBKGE7vur5e1OkdxEVsWD1CYFlddpt/CjfQs1ey', 1, 'user2.png', 0, '2024-03-11 15:13:13'),
+(26, 1703689628, 'Fernando', 'teste2@gmail.com', '$2y$10$yAkDfepI6ubbkl0tHBluPOO6FOdJ6N6ODgi0/JZp8GZ6IwUfFrgzu', 3, 'user.png', 0, '2024-03-12 10:26:14'),
+(27, 1035944128, 'Miguel', 'teste3@gmail.com', '$2y$10$i2zHS8WBb0bgvZDzkSgE7./3LGOfUa2lGKJjhli9sOEmzlCk8fwRu', 1, 'pin booking.jpg', 0, '2024-03-12 16:38:55');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `mensagens`
+--
+ALTER TABLE `mensagens`
+  ADD PRIMARY KEY (`msg_id`);
 
 --
 -- Índices para tabela `utilizadores`
@@ -63,10 +110,16 @@ ALTER TABLE `utilizadores`
 --
 
 --
+-- AUTO_INCREMENT de tabela `mensagens`
+--
+ALTER TABLE `mensagens`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- AUTO_INCREMENT de tabela `utilizadores`
 --
 ALTER TABLE `utilizadores`
-  MODIFY `utilizador_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `utilizador_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
