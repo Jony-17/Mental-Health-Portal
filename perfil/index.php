5a98d5a7d4ef127aa03d7930ac902177491e4dd9
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-05-19 21:14:10 +0100

    Add files via upload

--- perfil/index.php
+++ perfil/index.php
@@ -185,7 +185,7 @@ if (isset($_SESSION['id_utilizador'])) {
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
