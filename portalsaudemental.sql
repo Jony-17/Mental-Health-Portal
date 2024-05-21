cf760cd2f3fbd2c2eaf286fac5ee019946e71895
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-05-21 17:58:21 +0100

    Add files via upload

--- portalsaudemental.sql
+++ portalsaudemental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Maio-2024 às 14:32
+-- Tempo de geração: 21-Maio-2024 às 18:58
 -- Versão do servidor: 10.4.18-MariaDB
 -- versão do PHP: 8.0.3
 
@@ -156,7 +156,7 @@ INSERT INTO `exercicios_mindfulness_ex` (`exercicios_mindfulness_ex_id`, `exerci
 (6, 3, 'Posição de montanha (Tadasana)', 'yoga3.webp', '', ''),
 (7, 2, 'Ver um filme ou série', 'teste2.png', '', ''),
 (8, 2, 'Assistir/acompanhar algum desporto', 'teste2.png', '', ''),
-(29, 1, 'Mindfulness – Atenção à Respiração', '', '', 'ex1'),
+(29, 1, 'Mindfulness – Atenção à Respiração', '', 0x4d696e6466756c6e65737320e28093204174656ec3a7c3a36f20c3a02052657370697261c3a7c3a36f2e6d7033, 'ex1'),
 (30, 1, 'Mindfulness – Body Scan', '', 0x4d696e6466756c6e65737320e2809320426f6479205363616e2e6d7033, ''),
 (32, 1, 'Mindfulness – Explorar Sensações e Pensamentos Difíceis', '', 0x4d696e6466756c6e65737320e28093204578706c6f7261722053656e7361c3a7c3b5657320652050656e73616d656e746f7320446966c3ad636569732e6d7033, ''),
 (33, 1, 'Relaxamento Muscular Progressivo', '', 0x52656c6178616d656e746f204d757363756c61722050726f677265737369766f2e6d7033, ''),
@@ -587,19 +587,18 @@ CREATE TABLE `registos` (
   `sentimentos` varchar(1000) DEFAULT NULL,
   `quando` varchar(1000) DEFAULT NULL,
   `pensamento_alternativo` varchar(1000) DEFAULT NULL,
-  `comportamento_alternativo` varchar(1000) DEFAULT NULL,
-  `nota` varchar(1000) DEFAULT NULL
+  `comportamento_alternativo` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `registos`
 --
 
-INSERT INTO `registos` (`registos_id`, `utilizador_id`, `pensamento`, `comportamento`, `sentimentos`, `quando`, `pensamento_alternativo`, `comportamento_alternativo`, `nota`) VALUES
-(28, 26, 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', NULL),
-(29, 18, 'teste', 'teste', 'teste', 'teste', 'teste', 'teste2', NULL),
-(30, 18, 'testeteste', 'testeteste', 'testeteste', 'testeteste', 'testeteste', 'testeteste', NULL),
-(31, 18, 'testetesteteste', 'testetesteteste', 'testetesteteste', 'testetesteteste', 'testetesteteste', 'testetesteteste', NULL);
+INSERT INTO `registos` (`registos_id`, `utilizador_id`, `pensamento`, `comportamento`, `sentimentos`, `quando`, `pensamento_alternativo`, `comportamento_alternativo`) VALUES
+(28, 26, 'teste', 'teste', 'teste', 'teste', 'teste', 'teste'),
+(29, 18, 'teste', 'teste', 'teste', 'teste', 'teste', 'teste2'),
+(30, 18, 'testeteste', 'testeteste', 'testeteste', 'testeteste', 'testeteste', 'testeteste'),
+(31, 18, 'testetesteteste', 'testetesteteste', 'testetesteteste', 'testetesteteste', 'testetesteteste', 'testetesteteste');
 
 -- --------------------------------------------------------
 
@@ -654,7 +653,8 @@ INSERT INTO `utilizadores` (`utilizador_id`, `unique_id`, `nome`, `email`, `pass
 (12, 0, 'Admin', 'admin@gmail.com', '$2y$10$qhuOmrEAp7dt2CkDG.RAiOqrT.Ief55PRldhZjDpzDuVejQv002k.', 1, 'user.png', 1, '2024-02-25 20:17:56'),
 (18, 1703689423, 'João', 'teste@gmail.com', '$2y$10$c9VNDLhd/qGIVewjjY63.OPp7RPHuxseY2ZbFO9xwDIyv7L4z6qgK', 1, 'user.png', 0, '2024-03-11 15:13:13'),
 (26, 1703689628, 'Fernando', 'teste2@gmail.com', '$2y$10$yAkDfepI6ubbkl0tHBluPOO6FOdJ6N6ODgi0/JZp8GZ6IwUfFrgzu', 3, 'user.png', 0, '2024-03-12 10:26:14'),
-(27, 1035944128, 'Miguel', 'teste3@gmail.com', '$2y$10$i2zHS8WBb0bgvZDzkSgE7./3LGOfUa2lGKJjhli9sOEmzlCk8fwRu', 1, 'pin booking.jpg', 0, '2024-03-12 16:38:55');
+(27, 1035944128, 'Miguel', 'teste3@gmail.com', '$2y$10$i2zHS8WBb0bgvZDzkSgE7./3LGOfUa2lGKJjhli9sOEmzlCk8fwRu', 1, 'pin booking.jpg', 0, '2024-03-12 16:38:55'),
+(29, 1414583001, 'teste', 'teste5@gmail.com', '$2y$10$go0lJUIIBajXpaszNN1n6O0Q0IIe08B5vLaTfuua1rOMLXvpFAUQi', 3, '', 0, '2024-05-21 17:21:48');
 
 --
 -- Índices para tabelas despejadas
@@ -942,7 +942,7 @@ ALTER TABLE `ted_talks`
 -- AUTO_INCREMENT de tabela `utilizadores`
 --
 ALTER TABLE `utilizadores`
-  MODIFY `utilizador_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `utilizador_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Restrições para despejos de tabelas
