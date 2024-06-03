0c11d758af90c25dc8323aa6f4abb9e12234a5b6
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-06-03 14:05:54 +0100

    Add files via upload

--- perturbacoes/grupo-perturbacoes/index.php
+++ perturbacoes/grupo-perturbacoes/index.php
@@ -1,4 +1,7 @@
 <?php
+
+include '../../includes/header.php';
+
 session_start();
 require_once ("../../conn/conn.php");
 
@@ -45,34 +48,6 @@ if (isset($_GET['nome'])) {
     }
 }
 ?>
-
-
-<!DOCTYPE html>
-<html class="selection:text-white selection:bg-orange-400">
-
-<head>
-    <title>Portal de Saúde Mental</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../imgs/logo.png">
-
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Saira+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <!-- SweetAlert2 JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
-</head>
 
 <body>
     <style>
