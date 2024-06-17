ed1a0571c94b968d4fa0c92f3d974eff02558152
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-06-17 11:28:45 +0100

    Update darkmode.js

--- darkmode/darkmode.js
+++ darkmode/darkmode.js
@@ -2,12 +2,17 @@
 let darkMode = localStorage.getItem('darkMode'); 
 
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const iconLight = document.querySelector('#dark-mode-icon-light');
+const iconDark = document.querySelector('#dark-mode-icon-dark');
 
 const enableDarkMode = () => {
   // 1. Add the class to the body
   document.body.classList.add('darkmode');
   // 2. Update darkMode in localStorage
   localStorage.setItem('darkMode', 'enabled');
+  // 3. Change the icon to dark mode
+  iconLight.style.display = 'none';
+  iconDark.style.display = 'block';
 }
 
 const disableDarkMode = () => {
@@ -15,6 +20,9 @@ const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
   // 2. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
+  // 3. Change the icon to light mode
+  iconLight.style.display = 'block';
+  iconDark.style.display = 'none';
 }
  
 // If the user already visited and enabled darkMode
@@ -28,7 +36,7 @@ darkModeToggle.addEventListener('click', () => {
   // get their darkMode setting
   darkMode = localStorage.getItem('darkMode'); 
   
-  // if it not current enabled, enable it
+  // if it not currently enabled, enable it
   if (darkMode !== 'enabled') {
     enableDarkMode();
   // if it has been enabled, turn it off  
