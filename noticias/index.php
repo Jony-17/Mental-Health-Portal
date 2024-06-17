e0bdf7bd4b3889368209d462b9436b6fc2c0e686
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-06-17 11:31:11 +0100

    Add files via upload

--- noticias/index.php
+++ noticias/index.php
@@ -226,11 +226,11 @@ if (isset($_SESSION['id_utilizador'])) {
     </ol>
 
     <div class="background1">
-        <img src="background1.png" alt="banner background" />
+        <img src="imgs/imgs-backgrounds/background1.png" alt="banner background" />
     </div>
 
     <div class="background2">
-        <img src="background2.png" alt="banner background" />
+        <img src="imgs/imgs-backgrounds/background2.png" alt="banner background" />
     </div>
 
     <div class="heading">
