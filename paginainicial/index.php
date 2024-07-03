ed88e2d42deee683013aa129ae867a7c57cf7231
João Araújo <73491681+Jony-17@users.noreply.github.com>
2024-07-03 09:28:54 +0100

    Add files via upload

--- paginainicial/index.php
+++ paginainicial/index.php
@@ -4,15 +4,15 @@ include '../includes/header.php';
 session_start();
 require_once ("../conn/conn.php");
 
-// Verifica se a sessão do usuário está definida
+// Verificar se a sessão do utilizador está definida
 if (isset($_SESSION['id_utilizador'])) {
 
-    // Se a sessão do usuário já estiver definida, você pode executar outras ações aqui
+    // Se a sessão do utilizador já estiver definida, echo
     echo "Sessão do utilizador já está definida. ID do utilizador: " . $_SESSION['id_utilizador'];
 
     $utilizador_id = $_SESSION['id_utilizador'];
 
-    // Consulta SQL para buscar o campo img_perfil
+    // Consulta SQL
     $query = "SELECT nome, img_perfil FROM utilizadores WHERE utilizador_id = $utilizador_id";
 
     $result = mysqli_query($conn, $query);
@@ -79,6 +79,7 @@ if (isset($_SESSION['id_utilizador'])) {
         <div class="navbar">
             <div class="logo">Portal de <br> Saúde Mental.</div>
 
+            <!-- Links -->
             <ul class="links">
                 <li><a href="../sobre-nos">Sobre Nós</a></li>
                 <li><a href="../perturbacoes">Perturbações</a></li>
@@ -92,9 +93,10 @@ if (isset($_SESSION['id_utilizador'])) {
                         <li><a href="../conteudo-educativo/ted-talks">TED Talks</a></li>
                     </ul>
                 </li>
-                </li>
             </ul>
 
+            <!-- Verificação da sessão do utilizador, caso tenha sessão iniciada apresentada a foto de perfil -->
+            <!-- Caso não tenha, apresenta o botão de Iniciar Sessão -->
             <?php if (!empty($_SESSION['id_utilizador'])): ?>
                 <li class="dropdown-container">
                     <div class="profile-dropdown">
@@ -106,8 +108,6 @@ if (isset($_SESSION['id_utilizador'])) {
                         <i class="fas fa-chevron-down" style="margin-right: 20px;"></i>
                         <ul class="dropdown-p">
                             <li><a href="../perfil/">Perfil</a></li>
-                            <!--<li><a href="#">Termos e Condições</a></li>
-                            <li><a href="#">Definições</a></li>-->
                         </ul>
                     </div>
                     <a class="btn" onclick="logout()">Terminar Sessão</a>
@@ -121,7 +121,7 @@ if (isset($_SESSION['id_utilizador'])) {
             </div>
         </div>
 
-
+        <!-- Dropdown telemóvel -->
         <div class="dropdown_menu">
             <li><a href="../sobre-nos">Sobre Nós</a></li>
             <li><a href="../perturbacoes">Perturbações</a></li>
@@ -136,6 +136,8 @@ if (isset($_SESSION['id_utilizador'])) {
                 </ul>
             </li>
 
+            <!-- Verificação da sessão do utilizador, caso tenha sessão iniciada apresentada a foto de perfil -->
+            <!-- Caso não tenha, apresenta o botão de Iniciar Sessão -->
             <?php if (!empty($_SESSION['id_utilizador'])): ?>
                 <li class="dropdown-trigger">
                     <a href="#">
@@ -147,8 +149,6 @@ if (isset($_SESSION['id_utilizador'])) {
                         <i class="fas fa-chevron-down"></i></a>
                     <ul class="dropdown">
                         <li><a href="../perfil/">Perfil</a></li>
-                        <!--<li><a href="#">Termos e Condições</a></li>
-                        <li><a href="#">Definições</a></li>-->
                     </ul>
                 </li>
                 <li><a class="btn" onclick="logout()">Terminar Sessão</a></li>
@@ -156,6 +156,7 @@ if (isset($_SESSION['id_utilizador'])) {
                 <li><a class="btn" href="../areacliente/login/">Iniciar Sessão</a></li>
             <?php endif ?>
 
+            <!-- Botão para logout -->
             <script>
                 function logout() {
                     Swal.fire({
@@ -184,14 +185,13 @@ if (isset($_SESSION['id_utilizador'])) {
     <!--Home-->
     <section class="home" id="home">
         <div class="home-banner-container">
+
+            <!-- Background -->
             <div class="home-bannerImage-container">
                 <img src="imgs/imgs-backgrounds/background1.png" alt="banner background" />
             </div>
 
-            <!--<div class="home-banner2Image-container">
-                <img src="imgs/imgs-backgrounds/background-nuvens.png" alt="banner background" />
-            </div>-->
-
+            <!-- Títulos -->
             <div class="home-text-section">
                 <h1 class="home-primary-heading">
                     Saúde Mental <br> é uma <span style="color: var(--main-color);">prioridade</span>
@@ -203,6 +203,8 @@ if (isset($_SESSION['id_utilizador'])) {
                     <i class="fas fa-arrow-down"></i> Sabe mais
                 </a>
             </div>
+
+            <!-- Background -->
             <div class="image-container2">
                 <img src="imgs/imgs-home/background.png" alt="" />
             </div>
@@ -213,6 +215,8 @@ if (isset($_SESSION['id_utilizador'])) {
     <!--About-->
     <section class="about" id="about">
         <div class="about-banner-container">
+
+            <!-- Backgrounds -->
             <div class="about-bannerImage2-container">
                 <img src="imgs/imgs-backgrounds/background2.png" alt="banner background" />
             </div>
@@ -225,6 +229,7 @@ if (isset($_SESSION['id_utilizador'])) {
                 <img src="imgs/imgs-about/image2.png" alt="" />
             </div>
 
+            <!-- Títulos -->
             <div class="about-text-section">
                 <h2 class="about-primary-heading">A nossa missão</h2>
                 <h1 class="primary-heading">
@@ -264,6 +269,8 @@ if (isset($_SESSION['id_utilizador'])) {
     <!--Tipos Perturbações Mentais-->
     <section class="perturbacoes" id="perturbacoes">
         <div class="perturbacoes-banner-container">
+
+            <!-- Títulos -->
             <h2 class="perturbacoes-primary-heading">Tipos</h2>
             <div class="perturbacoes-banner-container2">
                 <h1 class="perturbacoes-second-heading">
@@ -274,6 +281,7 @@ if (isset($_SESSION['id_utilizador'])) {
         </div>
         <?php
 
+        // Consulta SQL para obter 4 perturbações
         $query = "SELECT nome, img_perturbacao, img_perturbacao_circ FROM perturbacoes LIMIT 4";
         $result = mysqli_query($conn, $query);
 
@@ -309,6 +317,8 @@ if (isset($_SESSION['id_utilizador'])) {
 
     <!--Artigos científicos-->
     <section class="artigos" id="artigos">
+
+        <!-- Background e Títulos -->
         <div class="artigos-banner-container">
             <div class="artigos-bannerImage2-container">
                 <img src="imgs/imgs-backgrounds/background2.png" alt="banner background" />
@@ -318,6 +328,7 @@ if (isset($_SESSION['id_utilizador'])) {
         </div>
         <?php
 
+        // Consulta SQL para obter 3 artigos científicos
         $query = "SELECT artigos.juncao_perturbacoes_id, perturbacoes.nome AS perturbacao_nome, grupos_perturbacoes.nome AS grupo_nome, artigos.titulo, artigos.data_publicacao, artigos.autor, artigos.img_artigo 
         FROM artigos 
         INNER JOIN juncao_perturbacoes ON artigos.juncao_perturbacoes_id = juncao_perturbacoes.juncao_perturbacoes_id
@@ -379,6 +390,8 @@ if (isset($_SESSION['id_utilizador'])) {
 
     <!--Quizzes-->
     <section class="quizzes" id="quizzes">
+
+        <!-- Background e Títulos -->
         <div class="quizzes-banner-container">
             <div class="quizzes-bannerImage4-container">
                 <img src="imgs/imgs-backgrounds/background4.png" alt="banner background" />
@@ -388,6 +401,7 @@ if (isset($_SESSION['id_utilizador'])) {
         </div>
         <div class="card4-container">
             <?php
+            //Consulta SQL para obter 2 quizzes
             $query = "SELECT * FROM quiz_nome LIMIT 2";
             $query_run = mysqli_query($conn, $query);
 
@@ -425,6 +439,7 @@ if (isset($_SESSION['id_utilizador'])) {
 
     <!--Notícias-->
     <section class="noticias" id="noticias">
+        <!-- Background e Títulos -->
         <div class="noticias-banner-container">
             <div class="noticias-bannerImage2-container">
                 <img src="imgs/imgs-backgrounds/background2.png" alt="banner background" />
@@ -437,18 +452,19 @@ if (isset($_SESSION['id_utilizador'])) {
         </div>
 
 
+        <!-- Container para as 5 notícias -->
         <div class="card5-container">
             <?php
-            $query = "SELECT autor, titulo, img_noticia
-                              FROM noticias LIMIT 1";
+            //Consulta SQL para obter 1 notícia
+            $query = "SELECT autor, titulo, img_noticia FROM noticias LIMIT 1";
 
             $result = mysqli_query($conn, $query);
 
             if ($result && mysqli_num_rows($result) > 0) {
-                /*while ($row = mysqli_fetch_assoc($result)) {*/
                 $row = mysqli_fetch_assoc($result);
                 ?>
 
+                <!-- Container para a 1ª notícia uma vez que tem tamanho diferente das outras -->
                 <div class="card5">
                     <?php
 
@@ -471,11 +487,13 @@ if (isset($_SESSION['id_utilizador'])) {
             }
             ?>
 
+            <!-- Container para as 4 notícias -->
             <div class="card6-container">
+                <!-- Container para as 2 notícias de cima -->
                 <div class="card-group">
                     <?php
-                    $query2 = "SELECT autor, titulo, img_noticia
-                                FROM noticias LIMIT 1, 2";
+                    //Consulta SQL para obter 2 notícias 
+                    $query2 = "SELECT autor, titulo, img_noticia FROM noticias LIMIT 1, 2";
                     $result2 = mysqli_query($conn, $query2);
 
                     while ($row2 = mysqli_fetch_assoc($result2)) {
@@ -502,10 +520,12 @@ if (isset($_SESSION['id_utilizador'])) {
                     }
                     ?>
                 </div>
+
+                <!-- Container para as 2 notícias de baixo -->
                 <div class="card-group">
                     <?php
-                    $query3 = "SELECT autor, titulo, img_noticia
-                                 FROM noticias LIMIT 2 OFFSET 3";
+                    //Consulta SQL para obter 2 notícias 
+                    $query3 = "SELECT autor, titulo, img_noticia FROM noticias LIMIT 2 OFFSET 3";
                     $result3 = mysqli_query($conn, $query3);
 
                     while ($row3 = mysqli_fetch_assoc($result3)) {
@@ -538,6 +558,7 @@ if (isset($_SESSION['id_utilizador'])) {
 
     <!--Perguntas Frequentes-->
     <section class="perguntas" id="perguntas">
+        <!-- Background e Títulos -->
         <div class="perguntas-banner-container">
             <div class="perguntas-bannerImage5-container">
                 <img src="imgs/imgs-backgrounds/background5.png" alt="banner background" />
@@ -546,6 +567,7 @@ if (isset($_SESSION['id_utilizador'])) {
             <h1 class="perguntas-second-heading">Perguntas Frequentes</h1>
         </div>
         <?php
+        // Consulta SQL para obter todas as perguntas frequentes
         $query = "SELECT pergunta, resposta FROM perguntas";
         $result = mysqli_query($conn, $query);
 
@@ -555,7 +577,6 @@ if (isset($_SESSION['id_utilizador'])) {
                 <div class="question">
                     <h3><?php echo $row['pergunta']; ?></h3>
                     <svg width="15" height="10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path
                             d="M207 381.5L12.7 187.1c-9.4-9.4-9.4-24.6 0-33.9l22.7-22.7c9.4-9.4 24.5-9.4 33.9 0L224 284.5l154.7-154c9.4-9.3 24.5-9.3 33.9 0l22.7 22.7c9.4 9.4 9.4 24.6 0 33.9L241 381.5c-9.4 9.4-24.6 9.4-33.9 0z" />
                     </svg>
@@ -573,6 +594,7 @@ if (isset($_SESSION['id_utilizador'])) {
 
 
     <!--Newsletter-->
+    <!-- Caso não tenha sessão iniciada irá aparecer isto -->
     <?php if (empty($_SESSION['id_utilizador'])): ?>
         <section class="newsletter" id="newsletter">
             <div class="newsletter-container">
@@ -592,7 +614,6 @@ if (isset($_SESSION['id_utilizador'])) {
             class="fas fa-chevron-up"></i></button>
 
 
-
     <!--Footer-->
     <footer>
         <div class="footer-row">
@@ -602,7 +623,7 @@ if (isset($_SESSION['id_utilizador'])) {
             </div>
 
             <?php
-
+            // Consulta SQL para apresentar todas as perturbações
             $query = "SELECT nome, img_perturbacao FROM perturbacoes";
             $result = mysqli_query($conn, $query);
 
@@ -633,7 +654,7 @@ if (isset($_SESSION['id_utilizador'])) {
 
             <!--Artigos-->
             <?php
-
+            // Consulta SQL para apresentar o título de 3 artigos
             $query = "SELECT titulo FROM artigos LIMIT 3";
             $result = mysqli_query($conn, $query);
 
@@ -665,7 +686,7 @@ if (isset($_SESSION['id_utilizador'])) {
 
             <!--Notícias-->
             <?php
-
+            // Consulta SQL para apresentar o título de 3 notícias
             $query = "SELECT titulo FROM noticias LIMIT 3";
             $result = mysqli_query($conn, $query);
 
@@ -759,6 +780,7 @@ if (isset($_SESSION['id_utilizador'])) {
         </div>
     </footer>
 
+    <!-- Enquanto a página não dá o load completo, apresenta esta frase -->
     <div class="loader">A carregar...</div>
 
     <!--Chatbot-->
